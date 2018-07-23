7ebca2e5f475159440041d38eaa3de226fc07891
liuhuoer <951340249@qq.com>
2018-07-24 01:30:04 +0800

    submit ex9_26.cpp

--- ch09/ex9_26.cpp
+++ ch09/ex9_26.cpp
@@ -7,27 +7,25 @@ using std::cout;using std::endl;using std::vector;using std::list;
 int main()
 {
 	int ia[]={0,1,1,2,3,5,8,13,21,55,89};
-	vector<int> vi;
-	list<int> li;
-
-	for(auto i:ia)
-	{
-		vi.push_back(i);
-		li.push_back(i);
-	}
+	vector<int> vi(ia,end(ia));		//rewrite_1
+	list<int> li(ia.begin(),ia.end());	//rewrite_2
 
 	auto it=vi.begin();
 	while(it!=vi.end())
 	{
-		if(!(*it%2))
-			vi.erase(it);
+		if(*it&0x1)			//rewrite_3(if it is real, *it is odd,else *it is even)
+			it=vi.erase(it);	//err_1_ever
+		else
+			++it;			//err_2_ever
 	}
 
 	auto it2=li.begin();
 	while(it2!=li.end())
 	{
-		if(*it2%2)
-			li.erase(it2);
+		if(!(*it2&0x1))
+			it2=li.erase(it2);
+		else
+			++it2;
 	}
 
 	for(auto i:vi)
