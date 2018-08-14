8948a27379ec4b25e6122a8220ba38ca9dcdecc0
liuhuoer <951340249@qq.com>
2018-08-14 23:47:38 +0800

    submit ex13_14

---
+++ ch13/ex13_14.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using std::cout;using std::endl;
+
+class numbered
+{
+public:
+	numbered(){mysn=++count;}
+	numbered(const numbered &){mysn=++count;}
+
+	int mysn;
+	static int count;	//rewrite_:init must out of class;
+};
+
+int numbered::count=6;
+
+void f(numbered s)
+{
+	cout<<s.mysn<<endl;
+}
+
+int main()
+{
+	numbered a,b=a,c=b;
+	f(a);
+	f(b);
+	f(c);
+
+	return 0;
+} 
